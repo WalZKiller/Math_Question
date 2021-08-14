66369a81935d565878050c5aae2f1cfae734a7fe
Walid <walidwajdy@gmail.com>
2021-08-14 21:46:40 +0800

    2nd quest done

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+//Question 1:
+
+//basic round off function = DONE
 function RoundOff(number, decimal) {    
 
     answer = number.toFixed(decimal);
 
-    console.log(answer);
+    //console.log(answer);
 }
 
 RoundOff(5.4002, 3)
@@ -10,10 +13,38 @@ RoundOff(5.4035, 3)
 RoundOff(5.4173, 3)
 RoundOff(5.4085, 3)
 
-RoundOff(183.716, 0)
+RoundOff(183.716, 0);
 
-//macam mana aku nak buat bawah ni??
+//Question 2: Express 0.01204 in standard form.
 
-//RoundOff(18.475, 2) = 18.48
+// cara untuk solve number times (10 to the power of anything) = DONE
+function StandardFormAnswer(number, exponent) {
 
+    exponent = Math.pow(10, exponent);
+
+    answer = number * exponent;
+
+    console.log(answer); //= DONE
+
+}
+//cth: Calculate 1.204 times 10 to the power of negative 2:
+StandardFormAnswer(0.1204, -2);
+StandardFormAnswer(1.204, -2);
+StandardFormAnswer(0.1204, -3);
+StandardFormAnswer(1.204, -3);
+
+//#region nak buat function untuk dapatkan equation drpd answer
+/*function StandardForm(value) {
+
+    answer = Math.pow(10, 2) + "x" + value.toString();
+
+    console.log(answer);
+}
+
+StandardForm(0.01204);*/
+
+//target:
+// StandardForm (0.01204) = 1.204 * 0.01 
+//fck xleh sial
+//#endregion
 
