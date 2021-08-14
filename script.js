ce7560329397e66936403314ece39a27b4770159
WalZKiller <walidwajdy@gmail.com>
2021-08-14 18:37:27 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function RoundOff(number, decimal) {    
+
+    answer = number.toFixed(decimal);
+
+    console.log(answer);
+}
+
+RoundOff(5.4002, 3)
+RoundOff(5.4035, 3)
+RoundOff(5.4173, 3)
+RoundOff(5.4085, 3)
+
+RoundOff(183.716, 0)
+
+//macam mana aku nak buat bawah ni??
+
+//RoundOff(18.475, 2) = 18.48
+
+
