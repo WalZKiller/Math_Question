9a0a0bb869c3921e6ddce28ab697c515543da19e
WalZKiller <walidwajdy@gmail.com>
2021-08-16 18:34:18 +0800

    Q4 done

--- script.js
+++ script.js
@@ -25,19 +25,41 @@ function exponentialForm(value, decimal) {
     return x
 }
 
+//tukar any number to the standard form (ada masalah kalau value < 1)
 function StandardForm(value) {
-    //5900 in standard form = 5.9 x 10(3)
 
-    var x = value.toExponential();
-    //var y = 
+    //the whole exponent, with (berapa2) decimal place
+    var expo = value.toExponential(2);
+    //nak extract value power of only
+    var pow = expo.toString().slice(-1);
+    //exponent without value power of
+    var expoOnly = expo.toString().slice(0, 4)
 
-    answer = x.toString() + "x" + Math.pow(10, 2).toString();
+    answer = expoOnly + "×" + "10" + "" + "(power of " + pow + ")";
+
+    /* if(value < 1) {
+
+    } */
+
     return answer
 }
+
+/* var num = 0.0052;
+console.log(StandardForm(0.0052));
+console.log(num.toFixed); */
+
+function gramToKilogram(value) {
+
+    ans = value * (-1000);
+    return ans
+}
+
+function kilogramToGram(value) {
+
+    ans = value * (1000);
+    return ans
+}
 //END FUNCTION
-
-console.log(StandardForm(5900));
-
 
 //Question 1:
 console.log(RoundOff(5.4002, 3));
@@ -68,3 +90,14 @@ StandardForm(0.01204);*/
 
 //Question 3: 59020000 = 𝑝 × 10�.  State the value of p and of q.
 console.log(exponentialForm(59020000));
+
+//Question 4:
+var x = kilogramToGram(0.0049896);
+var y = x * 500;
+var z = y * 100;
+ans = StandardForm(z);
+
+console.log(ans);
+
+//Question 5
+
